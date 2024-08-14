175008a78bba816e5141f6fd273bfaf0ea79227a
Patrick Santos <patrick.sg.santos@gmail.com>
2024-08-14 15:19:03 -0300

    implement error messages for invalid or no files being uploaded

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -15,7 +15,18 @@ class ReportsController < ApplicationController
   end
 
   def create
-    uploaded_file = params[:report][:file]
+    uploaded_file = params[:report]&.dig(:file)
+
+    if uploaded_file.nil?
+      flash.now[:alert] = 'Nenhum arquivo foi enviado. Por favor, selecione um arquivo XML ou ZIP.'
+      return render :new, status: :unprocessable_entity
+    end
+
+
+    unless valid_file_type?(uploaded_file)
+      flash.now[:alert] = 'Tipo de arquivo inválido. Por favor, envie um arquivo XML ou ZIP.'
+      return render :new, status: :unprocessable_entity
+    end
 
     file_path = Rails.root.join('tmp', uploaded_file.original_filename)
     File.open(file_path, 'wb') do |file|
@@ -23,7 +34,6 @@ class ReportsController < ApplicationController
     end
 
     XmlProcessorJob.perform_async(file_path.to_s)
-
     redirect_to new_report_path, notice: 'Seu relatório esta sendo gerado...'
   end
 
@@ -43,5 +53,10 @@ class ReportsController < ApplicationController
 
   def document_params
     params.require(:report).permit(:file)
+  end
+
+  def valid_file_type?(file)
+    allowed_types = ['application/xml', 'text/xml', 'application/zip']
+    allowed_types.include?(file.content_type)
   end
 end
